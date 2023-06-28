bb4a8cfde6ce0fa579df43558a1a7ca9dc3439e3
Quoc Khanh <quockhanh2912.nguyen@gmail.com>
2023-06-28 23:25:43 +0700

    build login Page; Signup Page, forgot password Page and change Password Page

--- pikachu_education/lib/pages/answer_page.dart
+++ pikachu_education/lib/pages/answer_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pikachu_education/data/data_user.dart';
+
+import '../data/data_image.dart';
+import '../routes/page_name.dart';
 
 class AnswerPage extends StatefulWidget {
   const AnswerPage({super.key});
@@ -10,6 +14,50 @@ class AnswerPage extends StatefulWidget {
 class _AnswerPageState extends State<AnswerPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body:
+        SafeArea(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.arrow_back, size: 25),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text('3 Answers',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold)),
+                    ),
+                  )
+                ]),
+              ),
+              ListView.builder(
+                itemBuilder: (context, index) => item(lists[index]),
+                itemCount: lists.length,
+              )
+            ]),
+        ),
+    );
+  }
+
+  Widget item(User user) {
+    return Container(
+      width: 200,
+      height: 200,
+      color: Colors.red,
+      child: Column(
+        children: [
+          Row(
+            children: [Text('asdsaxzcajskldj')],
+          )
+        ],
+      ),
+    );
   }
 }
--- pikachu_education/lib/routes/route_management.dart
+++ pikachu_education/lib/routes/route_management.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pikachu_education/pages/answer_page.dart';
 import 'package:pikachu_education/pages/change_password_page.dart';
+import 'package:pikachu_education/pages/forgot_password_page/1_get_otp_page.dart';
+import 'package:pikachu_education/pages/forgot_password_page/2_verify_page.dart';
+import 'package:pikachu_education/pages/forgot_password_page/3_save_new_password_page.dart';
+import 'package:pikachu_education/pages/forgot_password_page/4_success_change+password_page.dart';
+import 'package:pikachu_education/pages/signup_page/signup_page.dart';
+import 'package:pikachu_education/pages/signup_page/signup_success_page.dart';
 import 'package:pikachu_education/routes/page_name.dart';
 import 'package:pikachu_education/pages/login_page.dart';
 
@@ -12,7 +18,7 @@ var generateRoute = (settings) {
       }
     case PageName.signupPage:
       {
-        return MaterialPageRoute(builder: (context) => const LoginPage());
+        return MaterialPageRoute(builder: (context) => const SignupPage());
       }
     case PageName.changePasswordPage:
       {
@@ -22,6 +28,26 @@ var generateRoute = (settings) {
     case PageName.answerPage:
       {
         return MaterialPageRoute(builder: (context) => const AnswerPage());
+      }
+    case PageName.getOtpPage:
+      {
+        return MaterialPageRoute(builder: (context) => const GetOTPPage());
+      }
+    case PageName.verifyPage:
+      {
+        return MaterialPageRoute(builder: (context) => const VerifyPage());
+      }
+    case PageName.saveNewPasswordPage:
+      {
+        return MaterialPageRoute(builder: (context) => const SaveNewPasswordPage());
+      }
+    case PageName.successChangePasswordPage:
+      {
+        return MaterialPageRoute(builder: (context) => const SuccessChangePasswordPage());
+      }
+    case PageName.signupSuccessPage:
+      {
+        return MaterialPageRoute(builder: (context) => const SignupSuccessPage());
       }
   }
 };
