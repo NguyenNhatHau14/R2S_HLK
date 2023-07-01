33f564bdf21159f33bc355eba1d3b8433745a3ac
NguyenNhatHau14 <pointhaulearnjava@gmail.com>
2023-07-01 17:33:50 +0700

    profile success

--- pikachu_education/lib/main.dart
+++ pikachu_education/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pikachu_education/pages/login_page.dart';
-import 'package:pikachu_education/pages/signup_page/profile_page.dart';
+import 'package:pikachu_education/pages/profile_page.dart';
+
 import 'package:pikachu_education/routes/route_management.dart';
 
 void main() {
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home: const ProfilePage(),
       onGenerateRoute: generateRoute,
