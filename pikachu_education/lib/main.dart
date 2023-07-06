642533b67528ed9915f102c9a1e04048d848b195
NguyenNhatHau14 <pointhaulearnjava@gmail.com>
2023-07-06 18:03:26 +0700

    merg Khánh

--- pikachu_education/lib/main.dart
+++ pikachu_education/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:pikachu_education/pages/detail_answer_with_like_page.dart';
-import 'package:pikachu_education/pages/signup_page/profile_page.dart';
-import 'package:pikachu_education/pages/list_answer_page.dart';
 import 'package:pikachu_education/pages/login_page.dart';
 import 'package:pikachu_education/routes/route_management.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferences.getInstance();
   runApp(const MyApp());
