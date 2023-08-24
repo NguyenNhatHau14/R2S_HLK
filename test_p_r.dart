4dfde28108433cab4362afab324befc46425fb5c
Cong Nguyen <congnguyent@nashtechglobal.com>
2023-08-24 22:02:16 +0700

    Sua comment

--- test_p_r.dart
+++ test_p_r.dart
@@ -3,8 +3,5 @@ import 'package:flutter/material.dart';
 class TestPR extends StatelessWidget {
   const TestPR({Key key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+  AAAAAAAAAAAA
 }
